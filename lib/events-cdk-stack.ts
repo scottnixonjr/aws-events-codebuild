31ac70f1f0dd3daf30fc116fb93b3a250d02f0d0
Scott Nixon <scott.nixon@stelligent.com>
2021-01-18 14:40:22 -0800

    Scheduled CodeBuild Job Example

--- lib/events-cdk-stack.ts
+++ lib/events-cdk-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import { CodeBuildProject } from "@aws-cdk/aws-events-targets";
+import { Schedule, Rule } from '@aws-cdk/aws-events';
+
 
 export class EventsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const proj = new codebuild.Project(this, 'MyProject', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'echo "Hello, CodeBuild!"',
+            ],
+          },
+        },
+      }),
+    });
+
+    const codebuildTarget = new CodeBuildProject(proj);
+  
+
+    new Rule(this, 'ScheduleRule', {
+      schedule: Schedule.cron({ minute: '20' }),
+      targets: [codebuildTarget],
+     });
+
   }
 }
